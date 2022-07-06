5ffd9c314d5405ef8af23e5612f890e46d065d38
Shiba <90543822+ThatSovietShiba@users.noreply.github.com>
2022-07-05 21:26:27 -0500

    Update Scripts.lua

--- Scripts.lua
+++ Scripts.lua
@@ -1,9 +1,9 @@
 local httpService = game:GetService('HttpService')
 
-local SaveManager = {} do
-	SaveManager.Folder = 'LinoriaLibSettings'
-	SaveManager.Ignore = {}
-	SaveManager.Parser = {
+local ScriptManager = {} do
+	ScriptManager.Folder = 'LinoriaLibSettings'
+	ScriptManager.Ignore = {}
+	ScriptManager.Parser = {
 		Toggle = {
 			Save = function(idx, object) 
 				return { type = 'Toggle', idx = idx, value = object.Value } 
@@ -56,74 +56,23 @@ local SaveManager = {} do
 		}
 	}
 
-	function SaveManager:SetIgnoreIndexes(list)
+	function ScriptManager:SetIgnoreIndexes(list)
 		for _, key in next, list do
 			self.Ignore[key] = true
 		end
 	end
 
-	function SaveManager:SetFolder(folder)
+	function ScriptManager:SetFolder(folder)
 		self.Folder = folder;
 		self:BuildFolderTree()
 	end
 
-	function SaveManager:Save(name)
-		local fullPath = self.Folder .. '/configs/' .. name .. '.json'
-
-		local data = {
-			objects = {}
-		}
-
-		for idx, toggle in next, Toggles do
-			if self.Ignore[idx] then continue end
-
-			table.insert(data.objects, self.Parser[toggle.Type].Save(idx, toggle))
-		end
-
-		for idx, option in next, Options do
-			if not self.Parser[option.Type] then continue end
-			if self.Ignore[idx] then continue end
-
-			table.insert(data.objects, self.Parser[option.Type].Save(idx, option))
-		end	
-
-		local success, encoded = pcall(httpService.JSONEncode, httpService, data)
-		if not success then
-			return false, 'failed to encode data'
-		end
-
-		writefile(fullPath, encoded)
-		return true
-	end
-
-	function SaveManager:Load(name)
-		local file = self.Folder .. '/configs/' .. name .. '.json'
-		if not isfile(file) then return false, 'invalid file' end
-
-		local success, decoded = pcall(httpService.JSONDecode, httpService, readfile(file))
-		if not success then return false, 'decode error' end
-
-		for _, option in next, decoded.objects do
-			if self.Parser[option.type] then
-				self.Parser[option.type].Load(option.idx, option)
-			end
-		end
-
-		return true
-	end
-
-	function SaveManager:IgnoreThemeSettings()
-		self:SetIgnoreIndexes({ 
-			"BackgroundColor", "MainColor", "AccentColor", "OutlineColor", "FontColor", -- themes
-			"ThemeManager_ThemeList", 'ThemeManager_CustomThemeList', 'ThemeManager_CustomThemeName', -- themes
-		})
-	end
-
-	function SaveManager:BuildFolderTree()
+	function ScriptManager:BuildFolderTree()
 		local paths = {
 			self.Folder,
 			self.Folder .. '/themes',
-			self.Folder .. '/configs'
+			self.Folder .. '/configs',
+			self.Folder .. '/Scripts'
 		}
 
 		for i = 1, #paths do
@@ -134,16 +83,16 @@ local SaveManager = {} do
 		end
 	end
 
-	function SaveManager:RefreshConfigList()
-		local list = listfiles(self.Folder .. '/configs')
+	function ScriptManager:RefreshScriptList()
+		local slist = listfiles(self.Folder .. '/Scripts')
 
-		local out = {}
-		for i = 1, #list do
-			local file = list[i]
-			if file:sub(-5) == '.json' then
+		local scripts = {}
+		for i = 1, #slist do
+			local file = slist[i]
+			if file:sub(-5) == '.lua' then
 				-- i hate this but it has to be done ...
 
-				local pos = file:find('.json', 1, true)
+				local pos = file:find('.lua', 1, true)
 				local start = pos
 
 				local char = file:sub(pos, pos)
@@ -153,78 +102,38 @@ local SaveManager = {} do
 				end
 
 				if char == '/' or char == '\\' then
-					table.insert(out, file:sub(pos + 1, start - 1))
+					table.insert(scripts, file:sub(pos + 1, start - 1))
 				end
 			end
 		end
 		
-		return out
+		return scripts
 	end
 
-	function SaveManager:SetLibrary(library)
+	function ScriptManager:SetLibrary(library)
 		self.Library = library
 	end
 
-	function SaveManager:BuildConfigSection(tab)
-		assert(self.Library, 'Must set SaveManager.Library')
+	function ScriptManager:BuildConfigSection(tab)
+		assert(self.Library, 'Must set ScriptManager.Library')
 
-		local section = tab:AddRightGroupbox('Configuration')
+		local section = tab:AddRightGroupbox('Scripts')
 
-		section:AddDropdown('SaveManager_ConfigList', { Text = 'Config list', Values = self:RefreshConfigList(), AllowNull = true })
-		section:AddInput('SaveManager_ConfigName',    { Text = 'Config name' })
+		section:AddDropdown('ScriptManager_ScriptList', { Text = 'Script list', Values = self:RefreshScriptList(), AllowNull = true })
 
 		section:AddDivider()
 
-		section:AddButton('Create config', function()
-			local name = Options.SaveManager_ConfigName.Value
-
-			if name:gsub(' ', '') == '' then 
-				return self.Library:Notify('Invalid config name (empty)', 2)
-			end
-
-			local success, err = self:Save(name)
-			if not success then
-				return self.Library:Notify('Failed to save config: ' .. err)
-			end
-
-			self.Library:Notify(string.format('Created config %q', name))
-
-			Options.SaveManager_ConfigList.Values = self:RefreshConfigList()
-			Options.SaveManager_ConfigList:SetValues()
-			Options.SaveManager_ConfigList:SetValue(nil)
-		end):AddButton('Load config', function()
-			local name = Options.SaveManager_ConfigList.Value
-
-			local success, err = self:Load(name)
-			if not success then
-				return self.Library:Notify('Failed to load config: ' .. err)
-			end
-
-			self.Library:Notify(string.format('Loaded config %q', name))
-		end)
-
-		section:AddButton('Save config', function()
-			local name = Options.SaveManager_ConfigList.Value
-
-			local success, err = self:Save(name)
-			if not success then
-				return self.Library:Notify('Failed to overwrite config: ' .. err)
-			end
-
-			self.Library:Notify(string.format('Saved config %q', name))
-		end)
-
 		section:AddButton('Refresh config list', function()
-			Options.SaveManager_ConfigList.Values = self:RefreshConfigList()
-			Options.SaveManager_ConfigList:SetValues()
-			Options.SaveManager_ConfigList:SetValue(nil)
+			Options.ScriptManager_ScriptList.Values = self:RefreshConfigList()
+			Options.ScriptManager_ScriptList:SetValues()
+			Options.ScriptManager_ScriptList:SetValue(nil)
 		end)
 
 
-		SaveManager:SetIgnoreIndexes({ 'SaveManager_ConfigList', 'SaveManager_ConfigName' })
+		ScriptManager:SetIgnoreIndexes({ 'ScriptManager_ConfigList', 'ScriptManager_ConfigName' })
 	end
 
-	SaveManager:BuildFolderTree()
+	ScriptManager:BuildFolderTree()
 end
 
-return SaveManager
+return ScriptManager
